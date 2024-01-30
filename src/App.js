1ba99c640223ebcee0cd37edd461ad4db182409e
Iris Yatskaer <iriska1160@gmail.com>
2024-01-30 16:03:53 +0200

    Add load more products by clicking btn cmp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Accordion } from "./components/accordion";
 import { ImgSlider } from "./components/img-slider";
+import { LoadMoreBtn } from "./components/load-more-btn";
 import { RandomColor } from "./components/random-color";
 import { StarRating } from "./components/star-rating";
 
@@ -8,13 +9,18 @@ export function App() {
   return (
     <div className="App">
       {/* <Accordion /> */}
+
       {/* <RandomColor /> */}
+
       {/* <StarRating numOfStars={10} /> */}
-      <ImgSlider
+
+      {/* <ImgSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+
+      <LoadMoreBtn />
     </div>
   );
 }
---
+++ src/components/load-more-btn/index.jsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export function LoadMoreBtn() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableBtn, setDisableBtn] = useState(false);
+
+  async function fetchProducts() {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const result = await response.json();
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [count]);
+
+  useEffect(() => {
+    if (products && products.length === 100) setDisableBtn(true);
+  }, [products]);
+
+  if (loading) {
+    return <div>Products are loading! Please wait...</div>;
+  }
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="btn-container">
+        <button disabled={disableBtn} onClick={() => setCount(count + 1)}>
+          Load more products
+        </button>
+        {disableBtn ? <p>No more products to show</p> : null}
+      </div>
+    </div>
+  );
+}
